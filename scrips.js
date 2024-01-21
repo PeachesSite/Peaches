33c34f2a37fd7fd5492ddb6f7bf61f2fb27f4854
PeachesSite <157323019+PeachesSite@users.noreply.github.com>
2024-01-21 23:35:45 +0100

    Peaches

---
+++ scrips.js
@@ -0,0 +1,6 @@
+// Get a reference to the audio element
+const audio = new Audio("path/to/background-music.mp3");
+// Set the loop attribute to true for looping
+audio.loop = true;
+// Play the background music
+audio.play();
